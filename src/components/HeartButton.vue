9f1446e6d837bd4edf9f3114acce41241e8024b5
Simone Willemse <simywillemse@gmail.com>
2022-08-27 16:22:56 +0200

    added FavePage styling

---
+++ src/components/HeartButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="HeartButton">
+    <img class="Heart" src="images\Vector.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HeartButton',
+});
+</script>
