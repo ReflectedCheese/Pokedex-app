250adbd09fc5692437473960f31f1ad1ab876763
Simone Willemse <simywillemse@gmail.com>
2022-08-28 21:18:20 +0200

    search works, need modal styling

--- quasar.config.js
+++ quasar.config.js
@@ -8,9 +8,7 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js
 
-
 /* eslint-disable @typescript-eslint/no-var-requires */
-
 
 const { configure } = require('quasar/wrappers');
 
@@ -23,7 +21,7 @@ module.exports = configure(function (ctx) {
           enabled: true,
           files: './src/**/*.{ts,tsx,js,jsx,vue}',
         },
-      }
+      },
     },
 
     // https://v2.quasar.dev/quasar-cli-webpack/prefetch-feature
@@ -32,15 +30,10 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
-    boot: [
-      
-      'axios',
-    ],
+    boot: ['axios'],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
-    css: [
-      'app.scss'
-    ],
+    css: ['app.scss'],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -85,28 +78,25 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
     devServer: {
       server: {
-        type: 'http'
+        type: 'http',
       },
       port: 8080,
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
     },
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-framework
     framework: {
-      config: {},
-
+      //config: {},
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
-
       // For special cases outside of where the auto-import strategy can have an impact
       // (like functional components as one of the examples),
       // you can manually specify Quasar components/directives to be available everywhere:
       //
       // components: [],
       // directives: [],
-
       // Quasar plugins
-      plugins: []
+      //plugins: [],
     },
 
     // animations: 'all', // --- includes all animations
@@ -121,17 +111,17 @@ module.exports = configure(function (ctx) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       maxAge: 1000 * 60 * 60 * 24 * 30,
-        // Tell browser when a file from the server should expire from cache (in ms)
+      // Tell browser when a file from the server should expire from cache (in ms)
 
       // chainWebpackWebserver (/* chain */) {},
 
       middlewares: [
         ctx.prod ? 'compression' : '',
-        'render' // keep this as last one
-      ]
+        'render', // keep this as last one
+      ],
     },
 
     // https://v2.quasar.dev/quasar-cli-webpack/developing-pwa/configuring-pwa
@@ -155,30 +145,30 @@ module.exports = configure(function (ctx) {
           {
             src: 'icons/icon-128x128.png',
             sizes: '128x128',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-256x256.png',
             sizes: '256x256',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-384x384.png',
             sizes: '384x384',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+            type: 'image/png',
+          },
+        ],
+      },
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/developing-cordova-apps/configuring-cordova
@@ -188,7 +178,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/developing-capacitor-apps/configuring-capacitor
     capacitor: {
-      hideSplashscreen: true
+      hideSplashscreen: true,
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/developing-electron-apps/configuring-electron
@@ -197,13 +187,11 @@ module.exports = configure(function (ctx) {
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Windows only
         // win32metadata: { ... }
       },
@@ -211,20 +199,20 @@ module.exports = configure(function (ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'pokedex-app'
+        appId: 'pokedex-app',
       },
 
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      chainWebpackMain (/* chain */) {
+      chainWebpackMain(/* chain */) {
         // do something with the Electron main process Webpack cfg
         // extendWebpackMain also available besides this chainWebpackMain
       },
 
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      chainWebpackPreload (/* chain */) {
+      chainWebpackPreload(/* chain */) {
         // do something with the Electron main process Webpack cfg
         // extendWebpackPreload also available besides this chainWebpackPreload
-      }
-    }
-  }
+      },
+    },
+  };
 });
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,23 +1,22 @@
 <template>
   <q-header class="bg-white">
     <q-toolbar id="toolbar">
-      <img class="Filter" src="images\Filter.png" />
-      <img class="ArrowUp" src="images\ArrowUp.png" />
-      <img class="ArrowDown" src="images\ArrowDown.png" />
+      <img
+        class="Filter"
+        src="images\Filter.png"
+        @click="showFilterModal = true"
+      />
+      <span class="sortIcons" @click="showSortModal = true">
+        <img class="ArrowUp" src="images\ArrowUp.png" />
+        <img class="ArrowDown" src="images\ArrowDown.png" />
+      </span>
     </q-toolbar>
   </q-header>
 
   <div id="pokedex">
     <div class="pokedexTitle">Pokédex</div>
-    <button @click="sortPokemonList('name', 'desc')">
-      sorteer op naam aflopend
-    </button>
-    <button @click="filterPokemonListByType('fire')">
-      filter op fire type
-    </button>
 
     <div class="searchPokemon">
-      <!-- <form @submit="pressed(search.toString())"> -->
       <q-input
         style="width: 343px"
         rounded
@@ -32,7 +31,6 @@
           <q-icon name="search" />
         </template>
       </q-input>
-      <!-- </form> -->
     </div>
 
     <div class="button-container">
@@ -61,6 +59,99 @@
       />
     </div>
   </div>
+  <div
+    class="modal"
+    :class="{ showModal: showFilterModal, hideModal: !showFilterModal }"
+  >
+    <div class="modal-content">
+      <div class="modal-header">
+        <span @click="showFilterModal = false" class="close">&times;</span>
+        <div class="sortTitle">Filter op</div>
+      </div>
+      <q-select
+        filled
+        id="typeForm"
+        v-model="selectedFilter"
+        :options="filterOptions"
+        label="Type Pokemon"
+      />
+      <div class="formTypeButton">
+        <button
+          class="toepassen"
+          @click="
+            showFilterModal = false;
+            filterPokemonListByType(selectedFilter);
+          "
+        >
+          Toepassen
+        </button>
+      </div>
+
+      <!-- @click="filterPokemonListByType('fire')"> -->
+    </div>
+  </div>
+  <div
+    class="modal"
+    :class="{ showModal: showSortModal, hideModal: !showSortModal }"
+  >
+    <div class="modal-content">
+      <div class="modal-header">
+        <span @click="showSortModal = false" class="close">&times;</span>
+        <div class="sortTitle">Sorteren op</div>
+
+        <div class="sortField">
+          <q-field rounded outlined class="q-pb-sm">
+            <template v-slot:control>
+              <div
+                class="self-center full-width no-outline"
+                tabindex="0"
+                @click="sortPokemonList('name', 'asc')"
+              >
+                Alfabetisch oplopend
+              </div>
+            </template>
+          </q-field>
+          <q-field rounded outlined class="q-pb-sm">
+            <template v-slot:control>
+              <div
+                class="self-center full-width no-outline"
+                tabindex="0"
+                @click="sortPokemonList('name', 'desc')"
+              >
+                Alfabetisch aflopend
+              </div>
+            </template>
+          </q-field>
+          <q-field rounded outlined class="q-pb-sm">
+            <template v-slot:control>
+              <div
+                class="self-center full-width no-outline"
+                tabindex="0"
+                @click="sortPokemonList('id', 'asc')"
+              >
+                Numeriek oplopend
+              </div>
+            </template>
+          </q-field>
+          <q-field rounded outlined class="q-pb-sm">
+            <template v-slot:control>
+              <div
+                class="self-center full-width no-outline"
+                tabindex="0"
+                @click="sortPokemonList('id', 'desc')"
+              >
+                Numeriek aflopend
+              </div>
+            </template>
+          </q-field>
+
+          <button class="toepassen" @click="showSortModal = false">
+            Toepassen
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -78,12 +169,12 @@ export default defineComponent({
     const router = useRouter();
     let pokemonId;
     const { getPokemonList } = PokemonService();
-
     const selectedPokemon = ref();
-
     const pokemonDetail = ref();
-    const pokemonList = ref();
+    const pokemonList = ref([]);
     const searchQuery = ref('');
+    const selectedFilter = ref('');
+    const filteredPokemonList = ref([]);
 
     function onNavigate(path: string) {
       router.push({ path }).catch(console.error);
@@ -95,25 +186,25 @@ export default defineComponent({
     }
     onMounted(async () => {
       pokemonList.value = await getPokemonList();
-      // @ts-ignore:next-line
+      filteredPokemonList.value = pokemonList.value;
     });
 
     const searchedPokemonList = computed(() => {
       if (!searchQuery.value) {
-        return pokemonList.value;
+        return filteredPokemonList.value;
       }
 
       if (parseInt(searchQuery.value)) {
-        // @ts-ignore:next-line
-        return pokemonList.value.filter((pokemon) => {
+        return filteredPokemonList.value.filter((pokemon) => {
           return (
+            // @ts-ignore:next-line
             pokemon.id.toString().indexOf(searchQuery.value.toLowerCase()) != -1
           );
         });
       }
-      // @ts-ignore:next-line
-      return pokemonList.value.filter((pokemon) => {
+      return filteredPokemonList.value.filter((pokemon) => {
         return (
+          // @ts-ignore:next-line
           pokemon.name.toLowerCase().indexOf(searchQuery.value.toLowerCase()) !=
           -1
         );
@@ -121,6 +212,24 @@ export default defineComponent({
     });
 
     return {
+      filterOptions: [
+        'grass',
+        'fire',
+        'water',
+        'bug',
+        'normal',
+        'poison',
+        'electric',
+        'ground',
+        'fairy',
+        'fighting',
+        'psychic',
+        'rock',
+        'ghost',
+        'ice',
+        'dragon',
+        'flying',
+      ],
       pokemonList,
       searchedPokemonList,
       selectedPokemon,
@@ -129,22 +238,26 @@ export default defineComponent({
       pokemonId,
       searchQuery,
       onNavigate,
+      filteredPokemonList,
+      selectedFilter,
     };
   },
   methods: {
     sortPokemonList(property: string, direction: string) {
       if (direction === 'asc') {
         // @ts-ignore:next-line
-        this.pokemonList = sort(this.pokemonList).asc(property);
+        this.filteredPokemonList = sort(this.filteredPokemonList).asc(property);
       } else {
         // @ts-ignore:next-line
-        this.pokemonList = sort(this.pokemonList).desc(property);
+        this.filteredPokemonList = sort(this.filteredPokemonList).desc(
+          property
+        );
       }
     },
 
     filterPokemonListByType(pokemonFilterType: string) {
       // @ts-ignore:next-line
-      this.pokemonList = this.pokemonList.filter((pokemon) => {
+      this.filteredPokemonList = this.pokemonList.filter((pokemon) => {
         return (
           // @ts-ignore:next-line
           pokemon.types.some((pokemonType) => {
@@ -153,6 +266,12 @@ export default defineComponent({
         );
       });
     },
+  },
+  data() {
+    return {
+      showFilterModal: false,
+      showSortModal: false,
+    };
   },
 
   components: { PokemonList, TrainerButton },
@@ -167,5 +286,144 @@ export default defineComponent({
 
 .Filter {
   margin-right: 0.6rem;
+}
+.sortTitle {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 20px;
+  letter-spacing: -0.408px;
+  margin-bottom: 1.3rem;
+  padding-left: 0.5rem;
+
+  /* Dark 1 */
+  color: #1f2029;
+}
+.modal {
+  position: fixed;
+  z-index: 2000;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.showModal {
+  visibility: visible;
+  opacity: 1;
+  transition: opacity 0.4s ease-in, visibility 0ms ease-in 0ms;
+}
+
+.hideModal {
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.4s ease-in, visibility 0ms ease-in 0.2s;
+}
+
+.modal-content {
+  position: fixed;
+  bottom: 0;
+  background-color: #fefefe;
+  width: 100%;
+  -webkit-animation-name: slideIn;
+  -webkit-animation-duration: 0.4s;
+  animation-name: slideIn;
+  animation-duration: 0.4s;
+}
+
+.sortItems {
+  width: 346px;
+  height: 40px;
+  left: 13px;
+  top: 518px;
+  cursor: pointer;
+  margin-bottom: 0.8rem;
+  background: #f7f7f9;
+  border-radius: 10px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 18px;
+  padding-left: 10px;
+
+  letter-spacing: -0.408px;
+
+  color: #000000;
+}
+
+.formTypeButton {
+  margin-bottom: 1rem;
+  margin-top: 1rem;
+  margin-left: 3rem;
+}
+.toepassen {
+  width: 343px;
+  height: 44px;
+  left: 16px;
+  top: 734px;
+  cursor: pointer;
+
+  background: #1f2029;
+  border-radius: 100px;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 20px;
+  letter-spacing: -0.408px;
+
+  color: #ffffff;
+}
+
+.close {
+  color: grey;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.modal-header {
+  padding: 30px 20px;
+  background-color: white;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 20px;
+  letter-spacing: -0.408px;
+
+  color: #1f2029;
+}
+
+@-webkit-keyframes slideIn {
+  from {
+    bottom: -300px;
+    opacity: 0;
+  }
+  to {
+    bottom: 0;
+    opacity: 1;
+  }
+}
+
+@keyframes slideIn {
+  from {
+    bottom: -300px;
+    opacity: 0;
+  }
+  to {
+    bottom: 0;
+    opacity: 1;
+  }
 }
 </style>
