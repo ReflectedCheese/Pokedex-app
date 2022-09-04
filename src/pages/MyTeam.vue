a0e7d36bffa49e99f57177a3a7d527cf025335c2
Simone Willemse <simywillemse@gmail.com>
2022-09-04 21:31:48 +0200

    added category and bio. teamPage still wip

--- src/pages/MyTeam.vue
+++ src/pages/MyTeam.vue
@@ -1,19 +1,62 @@
 <template>
-  <div class="faveContainer">
-    <div class="DetailTitle">Mijn Team</div>
+  <div class="pokedex">
+    <q-header class="transparent detailPageHeader">
+      <div>
+        <BackButton id="Back" @click="goBack()" />
+      </div>
+    </q-header>
+
+    <div class="pokemonlist-container">
+      <div class="faveTitle">Mijn Team</div>
+      <PokemonList
+        v-for="(myTeam, index) in myTeamList"
+        :key="index"
+        :image="myTeam.sprites.front_default"
+        :name="myTeam.name"
+        :id="myTeam.id"
+        :types="myTeam.types"
+        @click="setPokemon(myTeam)"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import PokemonList from 'src/components/PokemonList.vue';
+import { PokemonService } from 'src/services/pokemonService';
+import { Pokemon } from 'src/components/models';
 
 export default defineComponent({
   name: 'myTeam',
+  components: {
+    PokemonList,
+  },
+  setup() {
+    const router = useRouter();
+    const myTeamList = ref([]);
+    const pokemonList = ref([]);
+    const selectedPokemon = ref();
+    const { getPokemonList, getMyTeamIds } = PokemonService();
+    function setPokemon(selectPokemon: Pokemon) {
+      selectedPokemon.value = selectPokemon;
+      router.push({ path: `/${selectPokemon.id}` }).catch(console.error);
+    }
+
+    function goBack() {
+      router.go(-1);
+    }
+
+    onMounted(async () => {
+      const myTeamIds = getMyTeamIds();
+      pokemonList.value = await getPokemonList();
+      myTeamList.value = pokemonList.value.filter((pokemon: Pokemon) =>
+        myTeamIds.includes(pokemon.id)
+      );
+    });
+
+    return { myTeamList, setPokemon, goBack };
+  },
 });
 </script>
-
-<style scoped>
-.faveContainer {
-  padding-top: 50px;
-}
-</style>
--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -12,6 +12,14 @@ const PokemonService = () => {
     return doGet(`https://pokeapi.co/api/v2/pokemon/${id}`);
   }
 
+  function getPokemonSpecies(id: string) {
+    return doGet(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+  }
+
+  function getPokemonEvolution(id: string) {
+    return doGet(`https://pokeapi.co/api/v2/evolution-chain/${id}`);
+  }
+
   const Types = [
     'fire',
     'normal',
@@ -51,14 +59,37 @@ const PokemonService = () => {
     favoritesArray = favoritesArray.filter((id) => id != pokemon.id);
     localStorage.setItem('favorites', JSON.stringify(favoritesArray));
   }
+  // MyTeam
+
+  function getMyTeamIds() {
+    // @ts-ignore:next-line
+    return JSON.parse(localStorage.getItem('myTeam'));
+  }
+
+  function addToMyTeam(pokemon: Pokemon) {
+    const myTeamArray: Array<number> = getMyTeamIds() ? getMyTeamIds() : [];
+    myTeamArray.push(pokemon.id);
+    localStorage.setItem('myTeam', JSON.stringify(myTeamArray));
+  }
+
+  function removeFromMyTeam(pokemon: Pokemon) {
+    let myTeamArray: Array<number> = getMyTeamIds() ? getMyTeamIds() : [];
+    myTeamArray = myTeamArray.filter((id) => id != pokemon.id);
+    localStorage.setItem('myTeam', JSON.stringify(myTeamArray));
+  }
 
   return {
     getPokemonList,
     getPokemonDetail,
+    getPokemonSpecies,
+    getPokemonEvolution,
     Types,
     getFavoritesIds,
     addToFavorites,
     removeFromFavorites,
+    getMyTeamIds,
+    addToMyTeam,
+    removeFromMyTeam,
   };
 };
 
